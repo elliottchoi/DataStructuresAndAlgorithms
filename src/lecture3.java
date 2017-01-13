0c80718cddeac85e66b56c4155c94bcadfd1de6d
Elliott Choi <elliottchoi98@gmail.com>
2017-01-12 20:20:35 -0500

    Finished Notes for Lecture 3 (Jan 12 2017)

--- src/lecture3.java
+++ src/lecture3.java
@@ -124,3 +124,84 @@ public class A2 {
 	}
 }
 
+//All Objects implicitly extend the java.lang.Object class
+
+//Abstract Class require a subclass to implement
+abstract class AbstractFoo {
+	public AbstractFoo() { }
+	// Abstract method:
+	public abstract void bar();
+	//This cannot be called 
+}
+
+class RealFoo extends AbstractFoo {
+public RealFoo() { super(); }
+//bar implements the abstract version of bar in super class
+public void bar() { }
+}
+
+//An interface is a pure virtual class, all methods are implicitly public and must be public
+interface IFoo {
+	// implicitly public
+	void foo();
+	}
+//IFoo can not be initialized because it is an interface (think pure virtual class)
+
+//Another Interface (pure virtual class)
+interface IBar {
+void bar();
+}
+
+//RealFoo implements two interfaces making for this idea of "multiple inheritence" 
+class RealFoo implements IFoo, IBar {
+@Overrides
+public void foo() { }
+@Overrides
+public void bar() { }
+}
+
+//An Example of Polymorphism 
+//Recall case of inheritence with method overriding, the method called depends on object type 
+//It will either be of super type, or derived type 
+
+//Encapsulation 
+//Tl;DR data hiding, keep as much data private, basically use methods to interact with class
+//actual data held in class is never exposed for the user to directly modify
+
+//Abstraction: Conditions of your method/design specification 
+//import static com.google.common.base.Preconditions.*;<-- this is java library you import
+public class A3 {
+// must specify non-null string and i > 10
+	public void check(@Nonnull String value, int i) {
+		checkNotNull(value,"value == null"); // NullPointerException <--native to imported library
+		checkArgument(i > 10,"i <= 10"); // IllegalArgumentException<--native to imported library
+		// do something with value and i
+		}
+}
+
+//Generic Types-->Java's version of C++ Templates
+interface Iterator<T> {//Iterator <T> T will define data type
+boolean hasNext();
+T next();
+}
+
+//Comparable <T> very similar to operator overloading 
+class Bar {}
+//Specifies we are comparing Foo and Bar data types
+class Foo implements Comparable<Foo>, Comparable<Bar> {
+	
+	private int v;
+	//Foo object 
+	Foo(int v) { this.v = v; }
+	
+	//Foo Comparison to Foo
+	public int compareTo(Foo other) {
+	checkNotNull(other); // make sure other is not null
+	return Integer.compareTo(this.v, other.v);
+	}
+	
+	//Foo Comparison to Bar
+	public int compareTo(Bar other) {
+	return -1; // this always < other
+	}
+}
